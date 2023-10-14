7d671a8986ee3ce7c6946514879a76015bdff983
Andrii <jaworovcki@gmail.com>
2023-10-15 01:41:57 +0300

    implement winning system

--- src/app/level2/level2.component.ts
+++ src/app/level2/level2.component.ts
@@ -33,7 +33,6 @@ export class Level2Component {
     return `occupied-${this.getPlayerName(col, row)}`;
   }
 
-  
   onRestart() {
     this.boardContent = [
       [BoardContent.EMPTY, BoardContent.EMPTY, BoardContent.EMPTY],
@@ -43,4 +42,38 @@ export class Level2Component {
     this.currentPlayerIndex = BoardContent.X;
     this.currentWinnerIndex = 0;
   }
+
+  getWinnerIndex(): number {
+    // check rows
+    for (let row = 0; row < 3; row++) {
+      if (this.boardContent[0][row] !== BoardContent.EMPTY &&
+        this.boardContent[0][row] === this.boardContent[1][row] &&
+        this.boardContent[0][row] === this.boardContent[2][row]) {
+        return this.boardContent[0][row];
+      }
+    }
+    // check columns
+    for (let col = 0; col < 3; col++) {
+      if (this.boardContent[col][0] !== BoardContent.EMPTY &&
+        this.boardContent[col][0] === this.boardContent[col][1] &&
+        this.boardContent[col][0] === this.boardContent[col][2]) {
+        return this.boardContent[col][0];
+      }
+    }
+    // check diagonals
+    if (this.boardContent[0][0] !== BoardContent.EMPTY &&
+      this.boardContent[0][0] === this.boardContent[1][1] &&
+      this.boardContent[0][0] === this.boardContent[2][2]) {
+      return this.boardContent[0][0];
+    }
+    if (this.boardContent[2][0] !== BoardContent.EMPTY &&
+      this.boardContent[2][0] === this.boardContent[1][1] &&
+      this.boardContent[2][0] === this.boardContent[0][2]) {
+      return this.boardContent[2][0];
+    }
+    return 0;
+  }
+
+
+
 }
